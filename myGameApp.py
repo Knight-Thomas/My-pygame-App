5b4f0df65a1013967840551f2009d886ba10f640
Knight-Thomas <thomasmilesknight@icloud.com>
2024-01-15 09:10:33 +0000

    Editing errors in the code

--- myGameApp.py
+++ myGameApp.py
@@ -3,141 +3,124 @@ import pygame as pg
 import random
 
 #parameters
-WIDTH, HEIGHT, FPS = (800,600,30)
-
+WIDTH, HEIGHT, FPS = (480,600,60)
 #define colours
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+YELLOW = (255,255,0)
 
-#sprite class
-'''a sprite will be an object which inherits from the built in sprite class'''
+#Initialise common pygame objects
+pg.init()
+pg.mixer.init()
+
+#create the display
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption('My Game')
+clock = pg.time.Clock()
+
+#a sprite will be an object which inherits from the built in sprite class
 class Player(pg.sprite.Sprite):
     #sprite for the player
     def __init__(self):
         #constructor
-        pg.sprite.Sprite.__init__(self)
-        '''inheritance'''
-        self.image = pg.Surface((50,50))
-        '''surface gives you something to draw on'''
+        pg.sprite.Sprite.__init__(self) #inheritance
+        self.image = pg.Surface((50,40))
         self.image.fill(GREEN)
-        '''useful for moving, size, position and collision'''
-        self.rect = self.image.get_rect()
-        '''looks at the image and gets its rect'''
-        self.rect.center = (WIDTH/2, HEIGHT/2)
-        '''places image in the centre'''
+        #useful for moving, size, position and collision
+        self.rect = self.image.get_rect()  #looks at the image and gets its rect
+        self.rect.centerx = WIDTH/2 #places image in the centre
+        self.rect.bottom = HEIGHT-10 #puts it in 10px from bottom of screen
+        #it needs to move side to side so we need speed
         self.speedx = 0
-    
+
     def update(self):
-        #move the sprite
-        '''we will keep the default speed of the object to zero and only after it with a key press
-        this way we avoid coding for what happens when the hey is released'''
+        #we will keep the default speed of object to zero and only alter it with a key press
+        #this way we avoid coding for what happens when the key is released
         self.speedx = 0
-        keystate = pg.key.get_pressed()
-        '''returned a list of keys that are down'''
+        keystate = pg.key.get_pressed() #returned a list of keys that are down
         if keystate[pg.K_LEFT]:
             self.speedx = -5
         if keystate[pg.K_RIGHT]:
             self.speedx = 5
-        self.rect.x += self.speedx
-        '''move at speed to be set by controls'''
-        if self.rect.left > WIDTH:
+        self.rect.x += self.speedx #move at speed to be set by controls
+        #to ensure it does not run off the screen
+        if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
-    
     def shoot(self):
         #spawns new bullet at centerx of player
-        #y will spawn at the top - i.e.bottom of the bulletat the top of the player
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        #add bullet to all sprites group so that its updated
+        #y will spawn at the top - i.e. bottom of the bullet at the top of the player
+        bullet = Bullet(self.rect.centerx,self.rect.top)
+        #add bullet to all sprites grouo so that its updated
         all_sprites.add(bullet)
         #add bullet to the bullets sprite group
         bullets.add(bullet)
 
-#initialise pygame contents
-pg.init()
-pg.mixer.init()
-'''mixer is required for sound'''
-
-#create the window
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-'''set mode takes one thing therefore it has two brackets'''
-pg.display.set_caption('My Game')
-clock = pg.time.Clock()
-'''handles the speed'''
-
 class Mob(pg.sprite.Sprite):
     #enemy mobile object which inherits from the sprite
     def __init__(self):
-        #constructor method
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((30,40))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-
-        #make the enemy spawn off top of the screen to appear off the screen and then start dropping down
-        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-        '''appears within the limits of the screen'''
-        self.rect.y = random.randrange(-100,-40)
-        '''this is off the screen'''
+       
+        #make the enemy spawn off top of screen to appear off thescreen and then start dropping down
+        self.rect.x = random.randrange(0,WIDTH - self.rect.width) #appears within the limits of the screen
+        self.rect.y = random.randrange(-100,-40) #this is off the screen
         self.speedy = random.randrange(1,8)
-
     def update(self):
         #move downwards
         self.rect.y += self.speedy
-        #deal with enemy when they get to the bottom of the screen
-        if self.rect.top > HEIGHT +10:
-            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-            '''appears within the limits of the screen and then start dropping down'''
-            self.rect.y = random.randrange(-100,-40)
-            '''this is off the screen'''
+        #deal with enemy when they get to bottom of the screen
+        if self.rect.top > HEIGHT + 10:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width) #appears within the limits of the screen
+            self.rect.y = random.randrange(-100,-40) #this is off the screen
             self.speedy = random.randrange(1,8)
 
 class Bullet(pg.sprite.Sprite):
     def __init__(self,x,y):
-        '''x and y are the respawn positions based on the player's position'''
+        #x and y and respawn positions based on the player's position
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((10,20))
-        self.image.fill(WHITE)
+        self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        #set re-spawn position to right infront of the player
+        #set respawn position to right in front of the player
         self.rect.bottom = y
         self.rect.centerx = x
         self.speedy = -10
-
     def update(self):
         #rect moves upwards at the speed
         self.rect.y += self.speedy
         #kill it if it moves off the top of the screen
         if self.rect.bottom < 0:
-            self .kill()
+            self.kill()
 
 #create a sprite group
 all_sprites = pg.sprite.Group()
-'''instantiate the player object and add it to the sprite group'''
-mobs = pg.sprite.Group()
-bullets = pg.sprite.Group()
-
+mobs = pg.sprite.Group()  #creating another group would aid during collision detection
+bullets = pg.sprite.Group()  
+#instatiate the player object and add it to the sprite group
 player = Player()
+#Spawn some mobs
 for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
 
 all_sprites.add(player)
+
 #game loop
 running = True
 while running:
-    '''keep the game runnin at the right speed'''
+    #keep the game running at the right speed
     clock.tick(FPS)
-    '''process input events (events)'''
-    '''You want to code the ability to close the game when the user quits'''
-    '''pygame will keep track of all events using an event loop'''
+    #process input (events)
     for event in pg.event.get():
-        '''check event for closing window'''
+        #check event for closing the window
         if event.type == pg.QUIT:
             running = False
         #check event for keydown to shoot
@@ -147,29 +130,24 @@ while running:
 
     #update
     all_sprites.update()
-
-    #check if a bullet hits a mob
-    '''you have to consider a grooup of bullets and a group of mobs
-       using a pygame.sprite.goupcollide() method helps to collide two groups together
-       setting the last two parameters will delete the bullet and the mob which collide withe each other
-       notice that this will kil the mobs so there needs to be a way of respawning them if they get killed'''
+    #Check if a bullet hits a mob
     hits = pg.sprite.groupcollide(mobs,bullets,True,True)
-    #check to see if a mob hit the player
-    for hit in hits:
-        m = Mob()
-        all_sprites.add(m)
-        mobs.add(m)
-        
-    hits = pg.sprite.spritecollide(player,mobs,False)
-    '''parametersare object to check against and group against'''
-    '''FALSE indicates wether hit item in group should be delete or not'''
+    #Check to see if a mob hits the player
+    hits = pg.sprite.spritecollide(player,mobs,False) #parameters are object to check against and group against
+    #False indicates whether hit item in group should be deleted or not
+   
+#respawn mobs destroyed by bullets
+for hit in hits:
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
     if hits:
         running = False
     #draw/render
-    screen.fill(BLUE)
-    '''always do this after drawing everything'''
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    #always do this after drawing anything
     pg.display.flip()
-
-#terminate the game window and close everythng up    
-pg.quit()
+#terminate the game window and close everything up    
+pg.quit
